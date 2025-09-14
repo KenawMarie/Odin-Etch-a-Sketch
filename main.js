27f2422705f87d16ee2dfdbe50fb2b3e2344d3f9
kenaw marie <204995551+KenawMarie@users.noreply.github.com>
2025-09-13 22:51:40 -0900

    add an event listener to change the background color of the divs

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-const div=document.querySelector('.container');
+const divs=document.querySelector('.container');
 let square=prompt('enter the number of rows and columns for the square?',16)
-let divs;
+let div;
 for (let i=0;i<(square)**2;i++){
-    divs=document.createElement('div');
-    divs.className='div';
-    divs.style.backgroundColor=`rgb(${number(256+1)},${number(256+1)},${number(256+1)})`
-    div.appendChild(divs)
+    div=document.createElement('div');
+    div.className='div';
+    divs.appendChild(div)
+    div.addEventListener('mouseenter',background)
+    div.addEventListener('mouseleave',background)
 }
 
-div.style.width=`${Number(square)*1.875 + (Number(square)-1)*0.125}rem`
-div.style.height=`${Number(square)*1.875 + (Number(square)-1)*0.125}rem`
-console.log((Number(square)+1)*30);
+divs.style.width=`${Number(square)*1.25 + (Number(square)-1)*0.125}rem`
+divs.style.height=`${Number(square)*1.25 + (Number(square)-1)*0.125}rem`
+
+
 function number(num){
     return Math.floor(Math.random()*num)
 }
+    function background(e){
+        e.target.style.backgroundColor=`rgb(${number(256+1)},${number(256+1)},${number(256+1)})`
+        console.log(e.target);
+    }
