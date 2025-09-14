845421f75a1e8416eff4a99fdedfe31801cf6cec
kenaw marie <204995551+KenawMarie@users.noreply.github.com>
2025-09-14 07:41:25 -0900

    add a button that prompts to input the number of sides for the square

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
+const body=document.querySelector('body')
 const divs=document.querySelector('.container');
-let square=prompt('enter the number of rows and columns for the square?',16)
-let div;
-for (let i=0;i<(square)**2;i++){
-    div=document.createElement('div');
+
+for (let i=0;i<(16)**2;i++){
+    let div=document.createElement('div');
     div.className='div';
     divs.appendChild(div)
     div.addEventListener('mouseenter',background)
     div.addEventListener('mouseleave',background)
 }
 
-divs.style.width=`${Number(square)*1.25 + (Number(square)-1)*0.125}rem`
-divs.style.height=`${Number(square)*1.25 + (Number(square)-1)*0.125}rem`
-
+divs.style.width='480px'
+divs.style.height='480px'
 
 function number(num){
     return Math.floor(Math.random()*num)
 }
-    function background(e){
-        e.target.style.backgroundColor=`rgb(${number(256+1)},${number(256+1)},${number(256+1)})`
-        console.log(e.target);
-    }
+function background(e){
+    e.target.style.backgroundColor=`rgb(${number(256+1)},${number(256+1)},${number(256+1)})`
+}
+const btn=document.createElement('button')
+btn.textContent='Number of Square'
+btn.className='btn'
+body.insertBefore(btn,divs)
+btn.addEventListener('click',popup)
+
+function popup(){
+    divs.textContent=''
+  let square= prompt('enter the number of squares per side for the new grid',16)
+  if(+square>100){
+      alert('enter numbers less than or equal to 100')
+  }
+  else{
+      for (let i=0;i<(square)**2;i++){
+          let div=document.createElement('div');
+          div.className='divs';
+          div.style.width=`${480/square}px`
+          div.style.height=`${480/square}px`
+          divs.appendChild(div)
+          div.addEventListener('mouseenter',background)
+          div.addEventListener('mouseleave',background)
+      }
+  }
+}
